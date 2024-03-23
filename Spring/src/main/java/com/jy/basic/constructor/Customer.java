296629bdf947706024f92110c61bb85ff39ac119
fly-1-1 <2673024411@qq.com>
2024-03-24 00:45:19 +0800

    构造注入重载

--- Spring/src/main/java/com/jy/basic/constructor/Customer.java
+++ Spring/src/main/java/com/jy/basic/constructor/Customer.java
@@ -7,6 +7,14 @@ public class Customer implements Serializable {
     private String name;
     private int age;
 
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public Customer(int age) {
+        this.age = age;
+    }
+
     public Customer(String name, int age) {
         this.name = name;
         this.age = age;
