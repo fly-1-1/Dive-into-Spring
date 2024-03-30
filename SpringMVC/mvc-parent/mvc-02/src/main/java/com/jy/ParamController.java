ba0bdb1cb1d369f73cdc2280b6450271a3e8e214
fly-1-1 <2673024411@qq.com>
2024-03-31 01:53:31 +0800

    简单变量接收请求参数

--- SpringMVC/mvc-parent/mvc-02/src/main/java/com/jy/ParamController.java
+++ SpringMVC/mvc-parent/mvc-02/src/main/java/com/jy/ParamController.java
@@ -10,6 +10,21 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("param")
 public class ParamController {
 
+
+    /*简单变量*/
+    @RequestMapping("param2")
+    public String m2(String name ,String password){
+        System.out.println("name = " + name + ", password = " + password);
+        return "param1";
+    }
+
+    /*简单变量的自动转换*/
+    @RequestMapping("param3")
+    public String m3(String name ,Integer password){
+        System.out.println("name = " + name + ", password = " + password);
+        return "param1";
+    }
+
     /*servlet api 接收请求参数*/
     @RequestMapping("param1")
     public String m1(HttpServletRequest request){
