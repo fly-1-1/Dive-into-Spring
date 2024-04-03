24ded781ea93ec50dbf073a112aa105502004cef
fly-1-1 <2673024411@qq.com>
2024-04-03 17:02:22 +0800

    运行环境文件拆分

---
+++ SpringBoot02_1/src/main/java/com/jy/springboot02_1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.jy.springboot02_1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello(){
+
+        return "hello";
+    }
+
+}
