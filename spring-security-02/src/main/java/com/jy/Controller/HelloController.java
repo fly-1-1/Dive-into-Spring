8b47cea6f2812310c966efb6b0493e95e6cdd8ca
fly-1-1 <2673024411@qq.com>
2024-04-07 05:02:01 +0800

    获取用户认证信息

--- spring-security-02/src/main/java/com/jy/Controller/HelloController.java
+++ spring-security-02/src/main/java/com/jy/Controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.jy.Controller;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         System.out.println("HelloController.hello");
+        //获取认证信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        System.out.println("身份信息:" + user.getUsername());
+        System.out.println("权限信息:" + authentication.getAuthorities());
+        new Thread(() -> {
+            Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+            System.out.println("子线程:" + authentication1);
+        }).start();
         return "hello";
     }
 
