3d9923982d7de5385a3c51c6d2e5cbea0e99b8df
fly-1-1 <2673024411@qq.com>
2024-04-13 04:22:31 +0800

    会话并发控制

---
+++ spring-security-10-session/src/main/java/com/jy/SpringSecurity10SessionApplication.java
@@ -0,0 +1,13 @@
+package com.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity10SessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity10SessionApplication.class, args);
+	}
+
+}
