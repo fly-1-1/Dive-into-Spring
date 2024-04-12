9cfaa465518776b64a32c32dac4a601fca5fa3b2
fly-1-1 <2673024411@qq.com>
2024-04-13 05:35:53 +0800

    csrf防御配置

---
+++ spring-security-11-csrf-blank/src/main/java/com/jy/SpringSecurity11CsrfBlankApplication.java
@@ -0,0 +1,13 @@
+package com.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity11CsrfBlankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity11CsrfBlankApplication.class, args);
+	}
+
+}
---
+++ spring-security-12-csrf-attck/src/main/java/com/jy/SpringSecurity12CsrfAttckApplication.java
@@ -0,0 +1,13 @@
+package com.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity12CsrfAttckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity12CsrfAttckApplication.class, args);
+	}
+
+}
