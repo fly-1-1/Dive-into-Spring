4cecdcf87bd49e6ff747b8a8569b095e2f36293b
fly-1-1 <2673024411@qq.com>
2024-04-12 17:30:46 +0800

    RememberMe安全持久化token

--- spring-security-08-rememberme/src/main/java/com/jy/SpringSecurity08RemembermeApplication.java
+++ spring-security-08-rememberme/src/main/java/com/jy/SpringSecurity08RemembermeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.jy.config")
 public class SpringSecurity08RemembermeApplication {
 
     public static void main(String[] args) {
--- spring-security-08-rememberme/src/main/java/com/jy/config/SecurityConfig.java
+++ spring-security-08-rememberme/src/main/java/com/jy/config/SecurityConfig.java
@@ -13,13 +13,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.util.UUID;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
@@ -34,8 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
@@ -44,19 +53,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .rememberMe()
-                .rememberMeServices(rememberMeServices())
+                //.rememberMeServices(rememberMeServices())
+                .tokenRepository(persistentTokenRepository())
                 .and()
                 .csrf().disable();
     }
 
-    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        //jdbcTokenRepository.setCreateTableOnStartup(true);//启动创建表结构
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+
+/*    @Bean
     public RememberMeServices rememberMeServices() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        //tokenRepository.setCreateTableOnStartup(true);
         return new PersistentTokenBasedRememberMeServices(
                 UUID.randomUUID().toString(),
                 userDetailsService(),
-                new InMemoryTokenRepositoryImpl());
-    }
-
-
+                tokenRepository);
+    }*/
 
 }
