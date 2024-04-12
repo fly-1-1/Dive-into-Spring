5198b89e210a54376204bc10a763e154a98d4d30
fly-1-1 <2673024411@qq.com>
2024-04-13 03:12:43 +0800

    RememberMe前后端分离

---
+++ spring-security-09-rememberme-split/src/main/java/com/jy/SpringSecurity09RemembermeSplitApplication.java
@@ -0,0 +1,13 @@
+package com.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity09RemembermeSplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity09RemembermeSplitApplication.class, args);
+    }
+
+}
---
+++ spring-security-09-rememberme-split/src/main/java/com/jy/config/MyPersistentTokenBasedRememberMeServices.java
@@ -0,0 +1,23 @@
+package com.jy.config;
+
+import org.springframework.core.log.LogMessage;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MyPersistentTokenBasedRememberMeServices extends PersistentTokenBasedRememberMeServices {
+
+    public MyPersistentTokenBasedRememberMeServices(String key, UserDetailsService userDetailsService, PersistentTokenRepository tokenRepository) {
+        super(key, userDetailsService, tokenRepository);
+    }
+
+    @Override
+    protected boolean rememberMeRequested(HttpServletRequest request, String parameter) {
+        String paramValue = request.getAttribute(parameter).toString();
+
+        return paramValue == null || !paramValue.equalsIgnoreCase("true") && !paramValue.equalsIgnoreCase("on")
+                && !paramValue.equalsIgnoreCase("yes") && !paramValue.equals("1");
+    }
+}
