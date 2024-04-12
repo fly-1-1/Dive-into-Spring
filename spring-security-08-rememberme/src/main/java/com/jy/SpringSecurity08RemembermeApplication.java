8c1bf0a1a7867d632611fca8cf00b2bde3e654d5
fly-1-1 <2673024411@qq.com>
2024-04-12 16:34:01 +0800

    RememberMe安全

--- spring-security-08-rememberme/src/main/java/com/jy/SpringSecurity08RemembermeApplication.java
+++ spring-security-08-rememberme/src/main/java/com/jy/SpringSecurity08RemembermeApplication.java
@@ -2,8 +2,10 @@ package com.jy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.jy.config")
 public class SpringSecurity08RemembermeApplication {
 
     public static void main(String[] args) {
--- spring-security-08-rememberme/src/main/java/com/jy/config/SecurityConfig.java
+++ spring-security-08-rememberme/src/main/java/com/jy/config/SecurityConfig.java
@@ -1,34 +1,40 @@
 package com.jy.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.util.UUID;
+
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("admin").password("{noop}123").roles("ADMIN").build());
         return manager;
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService());
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsServiceBean() throws Exception {
-        return super.userDetailsServiceBean();
-    }
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,9 +44,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .rememberMe()
-                .and()
-                .logout()
+                .rememberMeServices(rememberMeServices())
                 .and()
                 .csrf().disable();
     }
+
+    @Bean
+    public RememberMeServices rememberMeServices() {
+        return new PersistentTokenBasedRememberMeServices(
+                UUID.randomUUID().toString(),
+                userDetailsService(),
+                new InMemoryTokenRepositoryImpl());
+    }
+
+
+
 }
