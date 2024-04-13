7ba3f5dc67d0b96918ffa57fecb75e72d7fdfb34
fly-1-1 <2673024411@qq.com>
2024-04-14 02:26:35 +0800

    基于url授权

---
+++ spring-security-15-authorize/src/main/java/com/jy/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.jy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //创建内存数据源
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername("root").password("{noop}123").roles("ADMIN","USER").build());
+        inMemoryUserDetailsManager.createUser(User.withUsername("lisi").password("{noop}123").roles("USER").build());
+        inMemoryUserDetailsManager.createUser(User.withUsername("win7").password("{noop}123").authorities("READ_INFO").build());
+        return inMemoryUserDetailsManager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .mvcMatchers(HttpMethod.GET,"/admin","").hasRole("ADMIN") //具有 admin 角色
+                .mvcMatchers("/user").hasRole("USER")    //具有 user 角色
+                .mvcMatchers("/getInfo").hasAuthority("READ_INFO") //READ_INFO 权限
+                .antMatchers(HttpMethod.GET,"/admin").hasRole("ADMIN")
+                //.regexMatchers().hasRole()//正则匹配
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .and().csrf().disable();
+    }
+}
