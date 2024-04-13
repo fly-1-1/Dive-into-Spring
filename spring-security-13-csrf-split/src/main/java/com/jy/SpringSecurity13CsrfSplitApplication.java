8bcf87e9e75270b57cd1d7d2707f43cff990a155
fly-1-1 <2673024411@qq.com>
2024-04-13 22:55:46 +0800

    前后端分离csrf X-XSRF-TOKEN

---
+++ spring-security-13-csrf-split/src/main/java/com/jy/SpringSecurity13CsrfSplitApplication.java
@@ -0,0 +1,13 @@
+package com.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity13CsrfSplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity13CsrfSplitApplication.class, args);
+    }
+
+}
---
+++ spring-security-13-csrf-split/src/main/java/com/jy/config/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.jy.config.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jy.filter.LoginFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin").password("{noop}123").roles("admin").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    LoginFilter loginFilter() throws Exception {
+        LoginFilter loginFilter = new LoginFilter();
+        loginFilter.setFilterProcessesUrl("/doLogin");//指定认证 url
+        loginFilter.setUsernameParameter("uname");//指定接收json 用户名 key
+        loginFilter.setPasswordParameter("passwd");//指定接收 json 密码 key
+        loginFilter.setAuthenticationManager(authenticationManagerBean());
+        //认证成功处理
+        loginFilter.setAuthenticationSuccessHandler((req, resp, authentication) -> {
+            Map<String, Object> result = new HashMap<String, Object>();
+            result.put("msg", "登录成功");
+            result.put("用户信息", authentication.getPrincipal());
+            resp.setContentType("application/json;charset=UTF-8");
+            resp.setStatus(HttpStatus.OK.value());
+            String s = new ObjectMapper().writeValueAsString(result);
+            resp.getWriter().println(s);
+        });
+        //认证失败处理
+        loginFilter.setAuthenticationFailureHandler((req, resp, ex) -> {
+            Map<String, Object> result = new HashMap<String, Object>();
+            result.put("msg", "登录失败: " + ex.getMessage());
+            resp.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            resp.setContentType("application/json;charset=UTF-8");
+            String s = new ObjectMapper().writeValueAsString(result);
+            resp.getWriter().println(s);
+        });
+        return loginFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());//将令牌保存到cookie中 允许cookie前端获取 js获取
+
+
+        http.addFilterAt(loginFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+}
